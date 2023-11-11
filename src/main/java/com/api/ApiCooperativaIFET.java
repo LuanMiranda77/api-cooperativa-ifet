78c4ba2f2bfb3a834f08d7d62357d6435c15b3e8
LuanMiranda77 <luanprof30@gmail.com>
2023-11-11 17:59:22 -0300

    tete

--- src/main/java/com/api/ApiCooperativaIFET.java
+++ src/main/java/com/api/ApiCooperativaIFET.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SmartCommerceApi {
+public class ApiCooperativaIFET {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmartCommerceApi.class, args);
+		SpringApplication.run(ApiCooperativaIFET.class, args);
 		
 //		System.out.println(new BCryptPasswordEncoder().encode("123"));	
 		
